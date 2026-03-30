da3d85c0fb5298bb4cc8c6d57da203ece3df6355
tntromania <143605457+tntromania@users.noreply.github.com>
2026-03-30 14:02:33 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ app.post('/api/auth/google', async (req, res) => {
 
         if (!user) {
             const userCount = await User.countDocuments();
-            if (userCount >= 650) {
+            if (userCount >= 800) {
                 const dejaInLista = await Waitlist.findOne({ email: payload.email });
                 if (!dejaInLista) await Waitlist.create({ email: payload.email, name: payload.name });
                 return res.status(403).json({
